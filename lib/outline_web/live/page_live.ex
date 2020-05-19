62bb2ba2ed2d7c70c72b9718696744a868fc43e0
Derrick Zhang <derrick.m.zhang@gmail.com>
2020-05-19 22:16:30 +0800

    Extract list data from html

--- lib/outline_web/live/page_live.ex
+++ lib/outline_web/live/page_live.ex
@@ -1,39 +1,32 @@
 defmodule OutlineWeb.PageLive do
   use OutlineWeb, :live_view
 
+  @sample_list [
+    %{
+      title: "Shopping list",
+      children: [
+        %{
+          title: "Buy some milk",
+          children: []
+        },
+        %{
+          title: "Buy some beer",
+          children: []
+        }
+      ]
+    },
+    %{
+      title: "Read a book",
+      children: []
+    },
+    %{
+      title: "Make coffee",
+      children: []
+    }
+  ]
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
-  end
-
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
-
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  defp search(query) do
-    if not OutlineWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+    {:ok, assign(socket, list: @sample_list)}
   end
 end
